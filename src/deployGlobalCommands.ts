bce8cbad286ad79c0a3af92f0d8cc53460d51ba0
ReallyFullStack <218790890+ReallyFullStack@users.noreply.github.com>
2026-08-26 16:53:18 +0200

    fix(cmd deploying): better command deploying system

--- src/deployGlobalCommands.ts
+++ src/deployGlobalCommands.ts
@@ -3,7 +3,9 @@ import { REST } from '@discordjs/rest'
 import { RESTPostAPIApplicationCommandsJSONBody, Routes } from 'discord.js'
 import { readdirSync } from 'fs'
 import type ApplicationCommand from './templates/ApplicationCommand.js'
-const { TOKEN, CLIENT_ID } = process.env
+
+const { TOKEN, CLIENT_ID, DEFAULT_GUILD_ID } = process.env as Record<string, string>
+const rest = new REST().setToken(TOKEN)
 
 export default async function deployGlobalCommands() {
 	const commands: RESTPostAPIApplicationCommandsJSONBody[] = []
@@ -18,18 +20,13 @@ export default async function deployGlobalCommands() {
 		commands.push(commandData)
 	}
 
-	const rest = new REST({ version: '10' }).setToken(TOKEN as string)
-
 	try {
 		console.log('Started refreshing application (/) commands.')
 
-		await rest.put(Routes.applicationCommands(CLIENT_ID as string), {
-			body: [],
-		})
-
-		await rest.put(Routes.applicationCommands(CLIENT_ID as string), {
+		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, DEFAULT_GUILD_ID), {
 			body: commands,
 		})
+		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
 
 		console.log('Successfully reloaded application (/) commands.')
 	} catch (error) {
