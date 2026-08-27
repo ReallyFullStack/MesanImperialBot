2657fb1cf725c8837e8b00f56d3824bfc4c9ddea
ReallyFullStack <218790890+ReallyFullStack@users.noreply.github.com>
2026-08-27 15:00:11 +0200

    feat(docs): add caching of Markdown

--- src/docs.ts
+++ src/docs.ts
@@ -1,13 +1,52 @@
 import docs from '@googleapis/docs'
+import drive from '@googleapis/drive'
+import { existsSync } from 'fs'
+import { readFile, writeFile } from 'fs/promises'
+import { db, LawRecord } from './database.js'
+import { generateMarkdown } from './markdown.js'
 
 const auth = new docs.auth.GoogleAuth({
-	scopes: ['https://www.googleapis.com/auth/documents.readonly'],
+	scopes: [
+		'https://www.googleapis.com/auth/documents.readonly',
+		'https://www.googleapis.com/auth/drive.readonly',
+	],
 })
-const client = docs.docs({
+const docsClient = docs.docs({
 	version: 'v1',
-	auth: auth,
+	auth,
+})
+const driveClient = drive.drive({
+	version: 'v3',
+	auth,
 })
 
-export const getDocument = async (documentId: string) => {
-	return await client.documents.get({ documentId })
+export type Doc = docs.docs_v1.Schema$Document
+
+export async function getDocMarkdown(documentId: string): Promise<string> {
+	const { last_cached: lastCached } = db
+		.prepare('SELECT last_cached FROM laws WHERE ggdocs_id = ?')
+		.get(documentId) as Pick<LawRecord, 'last_cached'>
+	const { data: metadata } = await driveClient.files.get({
+		fileId: documentId,
+		fields: 'modifiedTime',
+	})
+	const lastModifiedTimestamp = Math.floor(Date.parse(metadata.modifiedTime as string) / 1000)
+
+	const cachePath = `../data/cache/${documentId}.md`
+
+	if (lastModifiedTimestamp < lastCached && existsSync(cachePath)) {
+		return await readFile(cachePath, 'utf-8')
+	}
+
+	const currentTimestamp = Math.floor(Date.now() / 1000)
+	const { data } = await docsClient.documents.get({ documentId })
+	const markdown = generateMarkdown(data)
+
+	await writeFile(cachePath, markdown, 'utf-8')
+	db.prepare('UPDATE laws SET last_cached = ? WHERE ggdocs_id = ?').run(
+		currentTimestamp,
+		documentId,
+	)
+
+	return markdown
 }
